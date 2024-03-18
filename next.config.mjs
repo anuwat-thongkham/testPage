8f3276a2f35f674a62efa418708e38b19023d1c2
“anuwat-thongkham” <anuwat.th@kkumail.com>
2024-03-18 22:43:52 +0700

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/testPage",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+};
 
 export default nextConfig;
