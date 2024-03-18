e7c60a2bb6d3d4450746a2946e7932ccb647bcfe
“anuwat-thongkham” <anuwat.th@kkumail.com>
2024-03-18 23:02:35 +0700

    change framework

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: "/testPage",
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-};
-
-export default nextConfig;
